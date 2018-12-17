54edeb0b1f938fcde00e721adf0ec283af32646f
Oleg Selin <sylvan.climb@gmail.com>
2018-12-17 23:03:31 +0300

    Added user's props

--- assets/utiles/api.js
+++ assets/utiles/api.js
@@ -2,11 +2,21 @@ const mocks = {
   'auth': { 'POST': { token: 'This-is-a-mocked-token' } },
   'user/me': {
     'GET': {
-      name: 'doggo',
+
       title: 'sir',
       email: 'mymail@mail.ru',
       phone: '12345678888',
-      bio: 'about me'
+      bio: 'about me',
+
+      surename: 'Phathers',
+      name: 'my name',
+      lastName: 'my second name',
+      birthday: '1986',
+
+      country: 'Russia',
+      region: 'region St.Petersburg',
+      city: 'St.Petersburg',
+      occupation: 'Data Soul'
     }
   }
 };
