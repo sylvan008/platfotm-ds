a098abefc20dab456d1c2edc4072c9b66bd1cef8
Oleg Selin <sylvan.climb@gmail.com>
2019-01-14 08:16:43 +0300

    Added fontawesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.3.1/css/all.css'},
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
