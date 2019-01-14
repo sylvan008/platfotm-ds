9c0cfe36b3ef83f0ce54d2d95d7a01f367c2d0d1
Oleg Selin <sylvan.climb@gmail.com>
2019-01-14 08:18:50 +0300

    Added upload methods

--- components/DsSubmitModal.vue
+++ components/DsSubmitModal.vue
@@ -14,41 +14,50 @@
         <b-tabs
           pills
           card>
+
+          <!-- File upload form -->
           <b-tab
             :title="$t('submitModal.ml.title')"
             active>
             <b-form>
               <b-form-group>
                 <b-form-file
-                  v-model="form.file"
-                  :state="Boolean(form.file)"
+                  v-model="uploadForm.file"
+                  :state="Boolean(uploadForm.file)"
                   :placeholder="$t('submitModal.ml.placeholder.load')"/>
+                <span :class="['upload-icon', 'fa', uploadForm.uploadIcon]" />
               </b-form-group>
               <b-form-group>
                 <b-input
+                  v-model="uploadForm.comment"
                   :placeholder="$t('submitModal.ml.placeholder.comment')"/>
               </b-form-group>
               <b-btn>{{ $t('submitModal.btns.submit') }}</b-btn>
             </b-form>
-            Selected file: {{ form.file && form.file.name }}<br>
-            File size: {{ form.file && form.file.type }}
+            Selected file: {{ uploadForm.file && uploadForm.file.name }}<br>
+            File size: {{ uploadForm.file && uploadForm.file.type }}
           </b-tab>
 
+          <!-- Form to send links -->
           <b-tab :title="$t('submitModal.prototype.title')">
             <b-form-group>
               <b-input
+                v-model="prototypeForm.name"
                 :placeholder="$t('submitModal.prototype.placeholder.name')"/>
             </b-form-group>
             <b-form-group>
               <b-input
+                v-model="prototypeForm.description"
                 :placeholder="$t('submitModal.prototype.placeholder.description')"/>
             </b-form-group>
             <b-form-group>
               <b-input
+                v-model="prototypeForm.link"
                 :placeholder="$t('submitModal.prototype.placeholder.link')"/>
             </b-form-group>
             <b-btn>{{ $t('submitModal.btns.submit') }}</b-btn>
           </b-tab>
+
         </b-tabs>
       </b-card>
     </b-modal>
@@ -66,16 +75,84 @@
     },
     data() {
       return {
-        form: {
-          file: null
+        uploadForm: {
+          file: null,
+          comment: '',
+          uploadIcon: 'fa-upload'
+        },
+        prototypeForm: {
+          name: '',
+          description: '',
+          link:''
         }
+      }
+    },
+    computed: {
+      fileUploadIndicateClasses() {
+        return {
+          uploadIndicate: true,
+
+        }
+      }
+    },
+    methods: {
+      async getSignedRequest(file){
+        try {
+
+          const validateFile = await this.$axios({
+            method: 'get',
+            url: '/sign_s3',
+            params: {
+              'file_name': file.name,
+              'file_type': file.type
+            },
+            withCredentials: true,
+            validateStatus(status) {
+              return status === 200
+            }
+          });
+
+          this.uploadFile(file, validateFile.data);
+
+        } catch (error) {
+          console.log(error);
+          alert("Невозможно проверить файл. Пожалуйста, повторите попытку.")
+        }
+
+
+      },
+      async uploadFile(file, s3Data) {
+        const postData = new FormData();
+        for (let key in s3Data.fields) {
+          if (s3Data.fields.hasOwnProperty(key)) {
+            postData.append(key, s3Data.fields[key])
+          }
+        }
+        postData.append('file', file);
+
+        await this.$axios({
+          method: 'post',
+          url: s3Data.url,
+          data: postData,
+          onUploadProgress(progressEvent) {
+
+          }
+        })
       }
     }
   }
 </script>
 
 <style>
-.submit-body {
+.upload-icon {
+  width: 40px;
+  height: 40px;
+  font-size: 15px;
+  border-radius: 50%;
+  color: white;
+  background-color: green;
+  text-align: center;
+  vertical-align: center;
   padding: 0;
 }
 
