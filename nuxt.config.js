2bb9096d81d325c1903db439cedaab23d4698311
Oleg Selin <sylvan.climb@gmail.com>
2019-01-17 08:10:19 +0300

    Added font awesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
+      { lang: 'ru', charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
