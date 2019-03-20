07b27569504d078e89d4e285dfa95644543186e7
Oleg Selin <sylvan.climb@gmail.com>
2019-03-20 12:43:36 +0300

    mock for competitionsPreview

--- assets/utiles/api.js
+++ assets/utiles/api.js
@@ -18,7 +18,31 @@ const mocks = {
       occupation: 'Data Soul' // Data Soul
     }
   },
-  'user/join': { 'POST': {join: true} }
+  'user/join': { 'POST': {join: true} },
+  'index': {'GET': {
+      remains_count: 1,
+      items: [
+        {
+          v: 2018.1,
+          title: 'comp-1-title',
+          cover_img: 'comp-1-img',
+          description: 'comp-1-descript',
+          date_begin: 'comp-1-dateBegin',
+          date_end: 'comp-1-dateEnd',
+          date_img: 'comp-1-dateImg',
+          prize: 'comp-1-prize',
+          prize_img: 'comp-1-prizeImg',
+          labels: [
+            { text: 'comp-1-labels-1', is_green: true },
+            { text: 'comp-1-labels-2', is_green: false },
+          ],
+          links: [
+            { text: 'comp-1-linkName-1', url: 'comp-1-url-1', img: 'comp-1-urlImg-1' },
+            { text: 'comp-1-linkName-2', url: 'comp-1-url-2', img: 'comp-1-urlImg-2' },
+          ]
+        }
+      ]
+    }}
 };
 
 const apiCall = ({ url, method }) => new Promise((resolve, reject) => {
@@ -30,7 +54,7 @@ const apiCall = ({ url, method }) => new Promise((resolve, reject) => {
     } catch (err) {
       reject(new Error(err));
     }
-  }, 1000)
+  }, 500)
 });
 
 export default apiCall;
